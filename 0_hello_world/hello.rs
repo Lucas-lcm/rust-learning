1da1095dff7fdc702795b664290bac4d35d9e5fc
Lucas Cardoso de Menezês <lucascmenezes00@gmail.com>
2024-12-28 21:32:45 -0300

    Add files via upload

---
+++ 0_hello_world/hello.rs
@@ -0,0 +1,7 @@
+//fn p declarar função
+//função main obrigatoria para programas binarios
+fn main(){ 
+    println!("Olá mundo") //println não é uma função é uma macro, toda macro termina com ! no final
+}
+
+//rustc e o nome do arquivo para compilar
